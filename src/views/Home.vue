c5f44bd73b6751d1764e28d7b166860aeda37a43
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2024-03-14 20:31:16 +0100

    test notes store

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,13 @@
 <template>
-  HELLO WORLD
+  {{ notes }}
 </template>
 
 <script setup>
   import { useTheme } from 'vuetify';
+  import { storeToRefs } from 'pinia';
+  import { useNotesStore } from '@/stores/notes';
 
   const theme = useTheme();
+  const notesStore = useNotesStore();
+  const { notes } = storeToRefs(notesStore);
 </script>
